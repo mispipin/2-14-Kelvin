b6911088ccde78bece7c5ca2992633d96ddb6ec2
mispipin <kelvinferdinan24@gmail.com>
2022-02-14 15:10:05 +0700

    modul 4

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,23 +1,24 @@
 @extends('layout\main')
 
 @section('container')
-<form>
 <div class="title-top-mb-5">
-    <h2 class="fw-bold">Contact Us</h2>
+      <h2 class="fw-bold">Contact Us</h2>
 </div>
-<div class="form-floating mb-3">
-  <input type="text" class="form-control" id="InputName" placeholder="nama">
-  <label for="InputName">nama</label>
-</div>
-<div class="form-floating mb-3">
-  <input type="email" class="form-control" id="inputemailt" placeholder="email">
-  <label for="inputemail" class="form-label">email</label>
-</div>
-<div class="form-floating">
-  <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea" style="height: 100px"></textarea>
-  <label for="floatingTextarea">Pesan</label>
-</div>
-<button type="button" class="btn btn-primary">submit</button>
+<form method="post" action="{{route('contacts.store')}}">
+  {{csrf_field()}}
+  <div class="form-floating mb-3">
+    <input type="text" class="form-control" id="InputName" placeholder="nama" name="nama">
+    <label for="InputName">nama</label>
+  </div>
+  <div class="form-floating mb-3">
+    <input type="email" class="form-control" id="inputemailt" placeholder="email" name="email">
+    <label for="inputemail" class="form-label">email</label>
+  </div>
+  <div class="form-floating">
+    <textarea class="form-control" placeholder="Tinggalkan pesan disini!" id="floatingTextarea" style="height: 100px" name="pesan"></textarea>
+    <label for="floatingTextarea">Pesan</label>
+  </div>
+  <button type="submit" class="btn btn-primary">kirim</button>
 </form>
 
 @endsection
